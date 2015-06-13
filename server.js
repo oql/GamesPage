9953b48b3ac11a16ec162a256eb5a0b0ba4fd6c5
ahesm <bitmapgames@naver.com>
2015-06-13 22:57:36 +0900

    games

---
+++ server.js
@@ -0,0 +1,63 @@
+var app = require('express')();
+var http = require('http').Server(app);
+
+app.get('/', function (req, res) {
+	res.sendfile( __dirname + '/front/index.html');
+});
+
+app.get('/form.css', function (req, res) {
+	res.sendfile( __dirname + '/front/form.css');
+});
+
+app.get('/form.js', function (req, res) {
+	res.sendfile( __dirname + '/front/form.js');
+});
+
+app.get('/intro', function (req, res) {
+	res.sendfile( __dirname + '/front/intro.html');
+});
+
+app.get('/intro.js', function (req, res) {
+	res.sendfile( __dirname + '/front/intro.js');
+});
+
+app.get('/notice', function (req, res){
+	res.sendfile( __dirname + '/front/notice.html');
+});
+
+app.get('/notice.css', function (req, res){
+	res.sendfile( __dirname + '/front/notice.css');
+});
+
+app.get('/notice.js', function (req, res){
+	res.sendfile( __dirname + '/front/notice.js');
+});
+
+app.get('/jquery', function (req, res){
+	res.sendfile( __dirname + '/front/jquery-1.11.1.js');
+});
+
+
+app.get('/res/logo.png', function (req, res){
+	res.sendfile( __dirname + '/front/res/logo.png');
+});
+
+app.get('/res/login.png', function (req, res){
+	res.sendfile( __dirname + '/front/res/login.png');
+});
+
+app.get('/res/banner.png', function (req, res){
+	res.sendfile( __dirname + '/front/res/banner.png');
+});
+
+
+
+var server = http.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
+
