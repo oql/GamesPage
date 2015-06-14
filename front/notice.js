7c2d441abd2f148e4773fb147ef6a14e875b58ae
Ggoggo <cfr2ak@gmail.com>
2015-06-14 15:08:01 +0900

    simple db connection added

--- front/notice.js
+++ front/notice.js
@@ -13,37 +13,37 @@ for(i=0;i<30;i++){
 
 
 $(document).ready(function(){
-	var zerocount = contents.length.toString().length - 1;
+	// var zerocount = contents.length.toString().length - 1;
 
-	for(i=0,pg=0;i<contents.length;i++){
-		var i_zcount = (i+1).toString().length - 1;
-		var zeros = "";
-		for(j=0;j<zerocount - i_zcount;j++) zeros += "0";
-		contents[i].number = zeros + (i+1);
-		pages[pg].push(contents[i]);
-		if(i%15==14) { pg++; pages[pg] = []; }
-	}
-	console.log(pages);
+	// for(i=0,pg=0;i<contents.length;i++){
+	// 	var i_zcount = (i+1).toString().length - 1;
+	// 	var zeros = "";
+	// 	for(j=0;j<zerocount - i_zcount;j++) zeros += "0";
+	// 	contents[i].number = zeros + (i+1);
+	// 	pages[pg].push(contents[i]);
+	// 	if(i%15==14) { pg++; pages[pg] = []; }
+	// }
+	// console.log(pages);
 
-	for(i=0;i<15;i++){
-			$("#noticebox ul:first-child").append("<li><div class='pcursor'>" + contents[i].number + "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp"
-			+ contents[i].cname + "</div><div><div>◀</div></div></li>");
-	}
+	// for(i=0;i<15;i++){
+	// 		$("#noticebox ul:first-child").append("<li><div class='pcursor'>" + contents[i].number + "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp"
+	// 		+ contents[i].cname + "</div><div><div>◀</div></div></li>");
+	// }
 
-	for(i=1;i<=9;i++){
-		$("<li class='pcursor'>"+ i +"</li>").insertBefore("#nboxindex ul li:last-child");
-		if(i>1&&i<9){
-			$("#nboxindex ul li:nth-child("+i+")").click(function(){
-					//$("#noticebox").css("transform","rotateY(180deg)");
-					//$("#noticebox2").css("transform","rotateY(360deg)");
-					for(i=0;i<15;i++){
-						$("#noticebox ul:last-child").append("<li><div class='pcursor'>" 
-							+ pages[$(this).text()-1][i].number + "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp"
-							+ pages[$(this).text()-1][i].cname + "</div><div><div>◀</div></div></li>");		
-					}
-			});
-		}
-	}
+	// for(i=1;i<=9;i++){
+	// 	$("<li class='pcursor'>"+ i +"</li>").insertBefore("#nboxindex ul li:last-child");
+	// 	if(i>1&&i<9){
+	// 		$("#nboxindex ul li:nth-child("+i+")").click(function(){
+	// 				//$("#noticebox").css("transform","rotateY(180deg)");
+	// 				//$("#noticebox2").css("transform","rotateY(360deg)");
+	// 				for(i=0;i<15;i++){
+	// 					$("#noticebox ul:last-child").append("<li><div class='pcursor'>"
+	// 						+ pages[$(this).text()-1][i].number + "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp"
+	// 						+ pages[$(this).text()-1][i].cname + "</div><div><div>◀</div></div></li>");
+	// 				}
+	// 		});
+	// 	}
+	// }
 
 	for(i=1;i<=$("#noticebox ul").children().length;i++){
 		$("#noticebox ul li:nth-child("+i+")").mouseenter(function(){
@@ -65,4 +65,3 @@ $(document).ready(function(){
 		});
 	}
 });
-
--- server.js
+++ server.js
@@ -23,7 +23,12 @@ app.get('/intro.js', function (req, res) {
 });
 
 app.get('/notice', function (req, res){
-	res.sendfile( __dirname + '/front/notice.html');
+	var query = connection.query('select * from doc',function(err,rows){
+		var response = genDoc(rows);
+		res.writeHead(200, {"Content-Type": "text/html"});
+		res.end(response);
+    });
+
 });
 
 app.get('/notice.css', function (req, res){
@@ -62,3 +67,130 @@ var server = app.listen(process.env.PORT||3000, function () {
 
 });
 
+
+app.get('/edit', function (req, res) {
+	res.sendfile( __dirname + '/front/edit.html');
+});
+
+var mysql = require("mysql");
+
+// fix user, password and database to fit in your system
+var connection = mysql.createConnection({
+	host: "localhost",
+	port: 3306,
+	user: "root",
+	password: "321654",
+	database: "test"
+});
+
+connection.connect(function(err){
+	if(err){
+		console.log("mysql connection error");
+		console.log(err);
+		throw err;
+	}
+});
+
+app.post('/saveArticle',function(req,res){
+    var user = {
+		'title':req.body.title,
+        'article':req.body.article
+	};
+
+    var query = connection.query('insert into users set ?',user,function(err,result){
+        if (err) {
+            console.error(err);
+            throw err;
+        }
+        res.send(200,'success');
+    });
+});
+
+function genDoc(data){
+	var response = '<!DOCTYPE html>' +
+	'<html>' +
+	'<head>' +
+	'	<meta charset="utf-8">' +
+	'	<link rel="stylesheet" type="text/css" href="/form.css"/>' +
+	'	<link rel="stylesheet" type="text/css" href="/notice.css"/>' +
+	'	<script src="/jquery"></script>' +
+	'	<script src="/form.js"></script>' +
+	'	<script src="/notice.js"></script>' +
+	'	<title>게임즈 공지사항</title>' +
+	'</head>' +
+	'<body>' +
+	'<div id="all">' +
+	'	<div id="sidemenu">' +
+	'		<ul id="sideback">' +
+	'			<li><div class="nodrag, pcursor">이곳은</div></li>' +
+	'			<li><div class="nodrag, pcursor">게임즈</div></li>' +
+	'			<li><div class="nodrag, pcursor">공지사항</div></li>' +
+	'			<li><div class="nodrag, pcursor">입니다.</div></li>' +
+	'		</ul>' +
+	'	</div>' +
+	'	<header>' +
+	'		<div id="login" class="pcursor"><div class="nodrag">로그이ㄴ</div></div>' +
+	'		<div><a href="/"><img class="nodrag" src="/res/logo.png"></a></div>' +
+	'	</header>' +
+	'	<nav>' +
+	'		<ul class="noselect">' +
+	'			<a href="/intro"><li>소개</li></a>' +
+	'			<a href="/notice"><li>공지사항</li></a>' +
+	'			<a href="#"><li>일감</li></a>' +
+	'			<a href="#"><li>게임</li></a>' +
+	'			<a href="#"><li>게시판</li></a>' +
+	'		</ul>' +
+	'	</nav>' +
+	'	<div id="contents">' +
+	'		<section>' +
+	'			<h1>공지사항</h1>' +
+	'			<hr>' +
+	'			<div id="noticebox">' +
+	'				<ul>' +
+	'					<li>&nbsp;번호&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;제목</li>';
+	for(i = 0; i < data.length; i++){
+		response += 	'<li>' +
+						'	<div class="pcursor">'
+							+ data[i].did + '&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp' + data[i].title +
+						'	</div>' +
+						'	<div>' +
+						'		<div>◀</div>' +
+						'	</div>' +
+						'</li>';
+	}
+	response += 	'</ul>' +
+	'				<ul>' +
+	'					<li>&nbsp;번호&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;제목</li>' +
+	'				</ul>' +
+	'			</div>' +
+	'			<div id="nboxindex">' +
+	'				<ul>' +
+	'					<li class="pcursor">◀</li>' +
+	'					<li class="pcursor">▶</li>' +
+	'				</ul>' +
+	'			</div>' +
+	'			<hr>' +
+	'		</section>' +
+	'	</div>' +
+	'	<footer>' +
+	'		copyleft (c) 2015 for everyone in the world' +
+	'	</footer>' +
+	'	<div id="loginlayout">' +
+	'		<div id="loginwindow">' +
+	'			<a href="#"><div id="exit" class="nodrag"><div>X</div></div></a>' +
+	'			<div id="loginimage"><img src="/res/login.png"></div>' +
+	'			<div>' +
+	'				*보안을 위해 아이디와 비밀번호를 10만 글자까지 입력가능하게 했습니다. <br>' +
+	'				<form method="POST">' +
+	'					<input type="text" name="id" size="70" maxlength="100000" placeholder="아이디"> <br>' +
+	'					<input type="password" name="password" size="70" maxlength="100000" placeholder="비밀번호"> <br>' +
+	'					<input type="submit" value="로그인/가입" style="width:523px;margin-right:5px;">' +
+	'				</form>' +
+	'			</div>' +
+	'		</div>' +
+	'	</div>' +
+	'</div>' +
+	'</body>' +
+	'</html>';
+	return response;
+}
