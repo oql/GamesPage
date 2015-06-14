cc3fbfc204b296efbce64a4c7163600c4624adf4
Ggoggo <cfr2ak@gmail.com>
2015-06-14 11:03:07 +0900

    static file response with regex

--- server.js
+++ server.js
@@ -6,51 +6,30 @@ app.get('/', function (req, res) {
 	res.sendfile( __dirname + '/front/index.html');
 });
 
-app.get('/form.css', function (req, res) {
-	res.sendfile( __dirname + '/front/form.css');
-});
-
-app.get('/form.js', function (req, res) {
-	res.sendfile( __dirname + '/front/form.js');
-});
 
 app.get('/intro', function (req, res) {
 	res.sendfile( __dirname + '/front/intro.html');
-});
-
-app.get('/intro.js', function (req, res) {
-	res.sendfile( __dirname + '/front/intro.js');
 });
 
 app.get('/notice', function (req, res){
 	res.sendfile( __dirname + '/front/notice.html');
 });
 
-app.get('/notice.css', function (req, res){
-	res.sendfile( __dirname + '/front/notice.css');
+app.get(/\/.*.css/, function(req, res){
+	res.sendFile(__dirname + "/front"+req.path);
 });
 
-app.get('/notice.js', function (req, res){
-	res.sendfile( __dirname + '/front/notice.js');
+app.get(/\/.*.js/, function(req, res){
+	res.sendFile(__dirname + "/front"+req.path);
+});
+
+app.get(/\/.*.png/, function(req, res){
+	res.sendFile(__dirname + "/front"+req.path);
 });
 
 app.get('/jquery', function (req, res){
 	res.sendfile( __dirname + '/front/jquery-1.11.1.js');
 });
-
-
-app.get('/res/logo.png', function (req, res){
-	res.sendfile( __dirname + '/front/res/logo.png');
-});
-
-app.get('/res/login.png', function (req, res){
-	res.sendfile( __dirname + '/front/res/login.png');
-});
-
-app.get('/res/banner.png', function (req, res){
-	res.sendfile( __dirname + '/front/res/banner.png');
-});
-
 
 
 var server = app.listen(process.env.PORT||3000, function () {
@@ -61,4 +40,3 @@ var server = app.listen(process.env.PORT||3000, function () {
   console.log('Example app listening at http://%s:%s', host, port);
 
 });
-
