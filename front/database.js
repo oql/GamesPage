9f2e831b25824e2ede2c77aa3fcd3261c598f07e
ahesm <bitmapgames@naver.com>
2015-07-26 20:11:44 +0900

    m

---
+++ front/database.js
@@ -0,0 +1,15 @@
+// var Sequelize = require('sequelize');
+// var database = new Sequelize('database', 'root', 'qwerty');
+
+// var user = database.define('User', {
+// 	id: Sequelize.STRING,
+// 	password: Sequelize.NUMBER
+// });
+
+// database.sync().then(function(){
+// 	return User.create({
+
+// 	});
+// }).then(function(){
+	
+// })
